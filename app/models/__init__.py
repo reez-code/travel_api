eed5bca0de00ecdc816b9db5aafcfa6bc0dad32c
Phil <phil.github@gmail.com>
2025-07-29 14:57:01 +0300

    initialised alembic and configured setup, created __init__.py file in model

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .user_query import UserQuery
+from .city import City
+from .message import Message
+from .hotel import Hotel
+from .transport_mean import TransportMean
+from .transport_option import TransportOption
+from .transport_route import TransportRoute
