e1c1cc80924cf9e5c4616cab642957e3e908296a
Phil <phil.github@gmail.com>
2025-07-29 13:25:06 +0300

    added transport means model

---
+++ app/models/transport_means.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from database import Base
+
+class TransportMean(Base):
+    __tablename__ = "transport_means"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+
+    # Relationships
+    transport_options = relationship("TransportOption", back_populates="means", cascade="all, delete-orphan")
+    user_queries = relationship("UserQuery", back_populates="transport")
